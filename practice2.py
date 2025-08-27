0b5d6a19e27509320f09d4152ebe4441c1e3870d
Shourya Upadhyay <upadhyayshourya352@gmail.com>
2025-08-27 14:55:01 +0530

    updated

--- practice2.py
+++ practice2.py
@@ -1,3 +1,4 @@
+#TASK1
 name = (input("ENTER THE USER NAME: "))
 print("HEY!",name , "WELCOME! TO THIS NEW USER INTERFACE TO MOVE FORWARD ENTER THE REQUIRED PASSWORD")
 inp_password = int(input("ENTER THE PASSWORD : "))
@@ -5,3 +6,21 @@ passward =  123456
 if passward == inp_password:
     print("Welcome back,", name)
 else: print("sorry wrong password")
+
+#TASK2
+list_good_std = ["piyush","shourya","tom","jay"]
+
+input_name = input(": ")
+
+if input_name in list_good_std:
+    print("you are a agood std")
+else: print("you are a bad std")
+
+#TASK3
+list_good_std = ["piyush","shourya","tom","jay"]
+input_name = input(": ")
+for i in range(len(list_good_std)):
+    if list_good_std[i] == input_name:
+        print("yes you are there")
+    else:
+        print("oops!")
